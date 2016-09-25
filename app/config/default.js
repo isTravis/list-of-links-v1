7ed9cf119a4f38d2a27fce4495af7cfc04162b4a
Travis Rich <travis.s.rich@gmail.com>
2016-09-24 21:17:14 -0400

    Removing boilerplate config folder

--- app/config/default.js
+++
@@ -1,3 +0,0 @@
-export default {
-  greeting: 'Hello, this app is running with default settings'
-};
--- app/config/production.js
+++
@@ -1,7 +0,0 @@
-import defaultConfig from './default';
-
-const config = {
-  greeting: 'Hello, this app is running with production settings'
-};
-
-export default Object.assign({}, defaultConfig, config);
