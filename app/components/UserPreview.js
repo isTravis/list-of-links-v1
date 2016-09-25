ae1d6557357bc13b839ba5d02d320d1bfb42df9e
Travis Rich <travis.s.rich@gmail.com>
2016-09-25 13:20:39 -0400

    UserPreview shows new link count

--- app/components/UserPreview.js
+++ app/components/UserPreview.js
@@ -2,21 +2,30 @@ import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
 export const UserPreview = React.createClass({
-  propTypes: {
-    user: PropTypes.object,
-  },
+	propTypes: {
+		user: PropTypes.object,
+	},
 
-  render: function() {
-    const user = this.props.user;
-    return (
-      <div>
-        <Link to={'/' + user.username}>
-          <img src={user.image} width="100px"/>
-          <div>{user.links.length}</div>
-        </Link>
-      </div>
-    );
-  }
+	render: function() {
+		const user = this.props.user || {};
+		const lastRead = user.Follow && user.Follow.lastRead;
+		const newLinkCount = user.links.reduce((previousVal, current) => {
+			if (current.createdAt > lastRead) {
+				return previousVal + 1;	
+			}
+			return previousVal;
+		}, 0);
+
+		return (
+			<div>
+				<Link to={'/' + user.username}>
+					<img src={user.image} width="100px" alt={'user'} />
+					<div>{user.links.length} | {newLinkCount}</div>
+
+				</Link>
+			</div>
+		);
+	}
 
 });
 
